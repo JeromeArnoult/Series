a5cf71a62f627598660f142e4d2235e1476929f2
JeromeArnoult <jerome.arnoult.pro@proton.me>
2024-02-14 16:41:10 +0100

    init et creation de la connexion à la BDD

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Serie;
+use App\Repository\SerieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -10,27 +14,63 @@ use Symfony\Component\Routing\Attribute\Route;
 class SerieController extends AbstractController
 {
     #[Route('/series', name: 'serie_list')]
-    public function list(): Response
+    public function list(SerieRepository $serieRepository): Response
     {
-        //Todo : Aller chercher les series en BDD
-        return $this->render('serie/list.html.twig', [
+        //L'utilisation du find by permet d'effectuer un trie
+        $series = $serieRepository->findBestSeries();
 
-        ]);
+        return $this->render('serie/list.html.twig',["series" => $series]);
     }
+
     #[Route('/series/details/{id}', name: 'serie_details')]
-    public function details (int $id): Response
+    public function details (int $id, SerieRepository $serieRepository): Response
     {
-        //Todo : Aller chercher les series en BDD
-        return $this->render('serie/details.html.twig', [
+        $serie = $serieRepository->find($id);
 
-        ]);
+        return $this->render('serie/details.html.twig',["serie" => $serie]);
     }
+
     #[Route('/series/create', name: 'serie_create')]
-    public function create (): Response
+    public function create (Request $request): Response
     {
+        dump($request);
+        return $this->render('serie/create.html.twig');
+    }
 
-        return $this->render('serie/create.html.twig', [
+    #[Route('/series/demo', name: 'serie_em-demo')]
+    //Inserer les données en base, on declare l'entityManager en argument
+    public function demo (EntityManagerInterface $entityManager): Response
+    {
+        //Création de l'instance de l'entité
+        $serie = new Serie();
 
-        ]);
+        //hydrate toutes les propriétés
+        $serie->setName('pif');
+        $serie->setBackdrop('dafsd');
+        $serie->setPoster('dafsdfd');
+        $serie->setDateCreated(new \DateTime());
+        $serie->setFirstAirDate(new \DateTime("- 1 year"));
+        $serie->setLastAirDate(new \DateTime("- 6 month"));
+        $serie->setGenres('drama');
+        $serie->setPopularity(123.00);
+        $serie->setVote(8.2);
+        $serie->setStatus("Canceled");
+        $serie->setTmdbId(329432);
+
+        dump($serie);
+        // Et on insert les données de mon objet serie en base
+        $entityManager->persist($serie);
+        $entityManager->flush();
+
+        dump($serie);
+
+        //Pour modifier un attribut:
+        $serie->setGenres('comedy');
+        $entityManager->flush();
+
+        //Supprimer mon object serie !!
+        $entityManager->remove($serie);
+        $entityManager->flush();
+        return $this->render('serie/demo.html.twig');
     }
 }
